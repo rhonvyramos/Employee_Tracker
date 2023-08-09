6a35c4e959c48ceece35c1c95a0cff68e6e482a1
rhonvyramos <rhonvyramos@gmail.com>
2023-08-09 10:54:38 -0400

    seed last employees table in business_db

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -18,3 +18,11 @@ VALUES
 ("Secret Keeper", 83.00, "7"),
 ("Brutalizer", 25.00, "4"),
 ("Assembly Programmer", 5.00, "3");
+
+-- seeds employees table
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+("Anastasia", "Tarnished-Eater", 1, 2),
+("Sellen", "Sorceress", 2, 4),
+("Mittens", "Ultimate Chessbot", 3, 2),
+("Godfrey", "The First Elden Lord", 4, 4);
