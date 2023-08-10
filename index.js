594164da35866ea3ca24c5d4175b35f5f6d4fdd2
rhonvyramos <rhonvyramos@gmail.com>
2023-08-10 09:48:55 -0400

    inquirer prompt restructure; add into table prompts are now in inquirer prompts js file; add into table inquirer now uses function call to get prompts for adding data into business_db tables

--- index.js
+++ index.js
@@ -34,28 +34,22 @@ function init() {
                 type: "list",
                 name: "overview",
                 message: "Enter an Employee Tracker option below...",
-                choices: inquirer_prompts,
+                choices: inquirer_prompts.prompt_choices,
                 loop: false
             }
         )
         .then((answers) => {
-            /*db_connection.connect( (err) => {
-                if (err) { console.log("Connection error."); console.log(err) }
-                console.log("MySQL server connection established on PORT: " + PORT);
-            });*/
-
             exit_tracker = answers.overview;
-            console.log(`You have chosen ${answers.overview}`);
             switch(answers.overview) {
-                case "View Departments": view_table("departments"); init(); break;
+                case "View Departments": view_table("departments"); break;
                 case "Add Department": add_into_table("departments"); break;
                 case "Update Department Data": department.update_department(); break;
     
-                case "View Roles": view_table("roles"); init(); break;
+                case "View Roles": view_table("roles"); break;
                 case "Add Role": add_into_table("roles"); break;
                 case "Update Role Data": role.update_role(); break;
     
-                case "View Employees": view_table("employees"); init(); break;
+                case "View Employees": view_table("employees"); break;
                 case "Add Employee": add_into_table("employees"); break;
                 case "Update Employee Data": employee.update_employee(); break;
 
@@ -65,17 +59,6 @@ function init() {
                     console.log("Employee Tracker exited.")
             };
         });
-
-        // terminates connection and code if exit_tracker equals the explicit exit tracker string
-        /*if(exit_tracker == "Exit Employee Tracker") { 
-            db_connection.end()
-            console.log("MySQL server connection terminated from PORT: " + PORT);
-            console.log("Employee Tracker exited.")
-            return;
-        }
-
-        // otherwise call init() again to prompt for options
-        init();*/
 };
 
 // function to view a business_db table based on choice from prompts
@@ -90,104 +73,7 @@ function view_table(table_name) {
 // function to add new data into a business_db table based on choice from prompts
 function add_into_table(table_name) {
     inquirer
-        .prompt([
-            // adding departments
-            {
-                type: "input",
-                name: "department_name",
-                message:"Input Department name... ->",
-                validate: (input, answers) => {
-                    return input ? true : console.log("Not a valid input.")
-                },
-                when: (answers) => {
-                    return table_name == "departments";
-                }
-            },
-
-            // adding roles
-            {
-                type: "input",
-                name: "title",
-                message:"Input new title... ->",
-                validate: (input, answers) => {
-                    return input ? true : console.log("Not a valid input.")
-                },
-                when: (answers) => {
-                    return table_name == "roles";
-                }
-            },
-
-            // regex syntax used from this link
-            // https://stackoverflow.com/questions/1779013/check-if-string-contains-only-digits
-            {
-                type: "input",
-                name: "salary",
-                message:"Input desired salary (##.##)... -> ",
-                validate: (input, answers) => {
-                    return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be an appropriate currency value."
-                },
-                when: (answers) => {
-                    return table_name == "roles";
-                }
-            },
-            {
-                type: "input",
-                name: "department_id",
-                message:"Input Department ID this role falls under (#)... -> ",
-                validate: (input, answers) => {
-                    return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be a singular number."
-                },
-                when: (answers) => {
-                    return table_name == "roles";
-                }
-            },
-
-            // adding employee
-            {
-                type: "input",
-                name: "first_name",
-                message:"Input Employee's first name... ->",
-                validate: (input, answers) => {
-                    return input ? true : console.log("Not a valid input.")
-                },
-                when: (answers) => {
-                    return table_name == "employees";
-                }
-            },
-            {
-                type: "input",
-                name: "last_name",
-                message:"Input Employee's last name... ->",
-                validate: (input, answers) => {
-                    return input ? true : console.log("Not a valid input.")
-                },
-                when: (answers) => {
-                    return table_name == "employees";
-                }
-            },
-            {
-                type: "input",
-                name: "role_id",
-                message:"Input Employee's Role ID... -> ",
-                validate: (input, answers) => {
-                    return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be an appropriate currency value."
-                },
-                when: (answers) => {
-                    return table_name == "employees";
-                }
-            },
-            {
-                type: "input",
-                name: "manager_id",
-                message:"Input Employee's Manager ID... -> ",
-                validate: (input, answers) => {
-                    return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be an appropriate currency value."
-                },
-                when: (answers) => {
-                    return table_name == "employees";
-                }
-            },
-        ])
+        .prompt(inquirer_prompts.add_into_table(table_name))
         .then((answers) => {
             console.log(answers);
             console.log("Added.")
--- js/inquirer_prompts.js
+++ js/inquirer_prompts.js
@@ -12,5 +12,108 @@ const prompt_choices = [
     "Exit Employee Tracker"
 ];
 
+function add_into_table (table_name) {
+    return prompts = 
+    [
+        // adding departments
+        {
+            type: "input",
+            name: "department_name",
+            message:"Input Department name... ->",
+            validate: (input, answers) => {
+                return input ? true : console.log("Not a valid input.")
+            },
+            when: (answers) => {
+                return table_name == "departments";
+            }
+        },
+    
+        // adding roles
+        {
+            type: "input",
+            name: "title",
+            message:"Input new title... ->",
+            validate: (input, answers) => {
+                return input ? true : console.log("Not a valid input.")
+            },
+            when: (answers) => {
+                return table_name == "roles";
+            }
+        },
+    
+        // regex syntax used from this link
+        // https://stackoverflow.com/questions/1779013/check-if-string-contains-only-digits
+        {
+            type: "input",
+            name: "salary",
+            message:"Input desired salary (##.##)... -> ",
+            validate: (input, answers) => {
+                return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be an appropriate currency value."
+            },
+            when: (answers) => {
+                return table_name == "roles";
+            }
+        },
+        {
+            type: "input",
+            name: "department_id",
+            message:"Input Department ID this role falls under (#)... -> ",
+            validate: (input, answers) => {
+                return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be a singular number."
+            },
+            when: (answers) => {
+                return table_name == "roles";
+            }
+        },
+    
+        // adding employee
+        {
+            type: "input",
+            name: "first_name",
+            message:"Input Employee's first name... ->",
+            validate: (input, answers) => {
+                return input ? true : console.log("Not a valid input.")
+            },
+            when: (answers) => {
+                return table_name == "employees";
+            }
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message:"Input Employee's last name... ->",
+            validate: (input, answers) => {
+                return input ? true : console.log("Not a valid input.")
+            },
+            when: (answers) => {
+                return table_name == "employees";
+            }
+        },
+        {
+            type: "input",
+            name: "role_id",
+            message:"Input Employee's Role ID... -> ",
+            validate: (input, answers) => {
+                return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be an appropriate currency value."
+            },
+            when: (answers) => {
+                return table_name == "employees";
+            }
+        },
+        {
+            type: "input",
+            name: "manager_id",
+            message:"Input Employee's Manager ID... -> ",
+            validate: (input, answers) => {
+                return /^\d+$/.test(input) ? true : message = "Not a valid input. Must be an appropriate currency value."
+            },
+            when: (answers) => {
+                return table_name == "employees";
+            }
+        },
+    ]
+    
+};
+
 // exports prompts for inquirer object
-module.exports = prompt_choices;
+module.exports = { prompt_choices, add_into_table };
